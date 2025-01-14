e5e284896c03be960e81eb311211e596ce8abc6b
Ahmad Raza <ahmadraza20082003@gmail.com>
2025-01-14 23:37:45 +0530

    Created Website

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-full h-[100vh] bg-slate-900 px-5 flex flex-col text-white items-center justify-center">
+      <h3 className='text-lg font-semibold uppercase'>Welcome</h3>
+      <h1 className='text-white text-center uppercase font-extrabold text-4xl sm:text-5xl'>Hotel Wale</h1>
+      <h2 className='mt-5 text-center'>We Are Working On Something Great For Hotel Industries Stay Tuned !</h2>
+    </div>
+  );
+}
+
+export default App;

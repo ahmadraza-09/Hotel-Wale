6c029de62f1dc18f22295c2707f132c2fea13b5d
Ahmad Raza <ahmadraza20082003@gmail.com>
2025-05-18 21:12:58 +0530

    added login modal

---
+++ src/components/login-modal.jsx
@@ -0,0 +1,152 @@
+import React, { useState } from "react";
+// import axios from "axios";
+
+const LoginModal = ({ isOpen, onClose }) => {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const [name, setName] = useState("");
+  const [dob, setDob] = useState("");
+  const [gender, setGender] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const resetForm = () => {
+    setName("");
+    setDob("");
+    setGender("");
+    setMobile("");
+    setEmail("");
+    setPassword("");
+    setError("");
+  };
+
+  const validateForm = () => {
+    if (!isLogin) {
+      if (!name || !dob || !gender || !mobile)
+        return "All fields are required for registration";
+    }
+    if (!email || !password) return "Email and Password are required";
+    return null;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const validationError = validateForm();
+    if (validationError) {
+      setError(validationError);
+      return;
+    }
+
+    // try {
+    //   if (isLogin) {
+    //     // const res = await axios.post("/api/login", { email, password, role });
+    //     alert("Logged in!");
+    //   } else {
+    //     // const res = await axios.post("/api/register", {
+    //     name,
+    //       gender,
+    //       mobile,
+    //       email,
+    //       password,
+    //       // });
+    //       alert("Registered!");
+    //   }
+    //   resetForm();
+    //   onClose();
+    // } catch (err) {
+    //   setError("Something went wrong");
+    // }
+  };
+
+  const handleOutsideClick = (e) => {
+    if (e.target.id === "modal") onClose();
+  };
+
+  if (!isOpen) return null;
+
+  return (
+    <div
+      id="modal"
+      onClick={handleOutsideClick}
+      className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 p-4"
+    >
+      <div className="bg-white sm:p-6 p-4 rounded-xl w-full max-w-md shadow-lg">
+        <h2 className="text-2xl font-semibold mb-4 text-center">
+          {isLogin ? "Welcome Back 👋" : "Register Now"}
+        </h2>
+        {error && (
+          <p className="text-red-600 text-sm mb-2 text-center">{error}</p>
+        )}
+
+        <form onSubmit={handleSubmit} className="space-y-4">
+          {!isLogin && (
+            <>
+              <input
+                type="text"
+                placeholder="Full Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="w-full px-4 py-2 border rounded"
+              />
+
+              <input
+                type="text"
+                placeholder="Mobile Number"
+                value={mobile}
+                onChange={(e) => setMobile(e.target.value)}
+                className="w-full px-4 py-2 border rounded"
+              />
+            </>
+          )}
+
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="w-full px-4 py-2 border rounded"
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="w-full px-4 py-2 border rounded"
+          />
+
+          <div className="flex gap-4">
+            <button className="w-[50%] bg-slate-200 rounded" onClick={onClose}>
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="w-[50%] bg-myColor text-white py-2 rounded hover:bg-myColor transition"
+            >
+              {isLogin ? "Login" : "Register"}
+            </button>
+          </div>
+        </form>
+
+        <div className="text-center mt-4">
+          <p className="text-sm">
+            {isLogin ? "Not registered yet?" : "Already have an account?"}
+            <button
+              onClick={() => {
+                setIsLogin(!isLogin);
+                setError("");
+              }}
+              className="text-blue-600 ml-2 hover:underline"
+            >
+              {isLogin ? "Register" : "Login"}
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginModal;
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,11 +4,13 @@ import Sidebar from "./sidebar"; // Import Sidebar
 import User from "../assets/icons/user-icon.svg";
 import MenuBar from "../assets/icons/menu-bar-icon.svg";
 import Logo from "../assets/logo.jpg";
+import LoginModal from "./login-modal";
 
 const Navbar = () => {
   const navigate = useNavigate();
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [loginModal, setLoginModal] = useState(true);
 
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -53,13 +55,19 @@ const Navbar = () => {
           >
             <path d="M176.54 34.58c-8.61-8.83-20.13-13.99-32.29-14.53c-21.53-.97-36.06 12.06-43.92 22.17c-7.86-10.12-22.39-23.14-43.92-22.17c-12.27.54-23.68 5.7-32.29 14.53c-8.72 8.93-13.45 20.56-13.45 32.83c0 19.27 10.98 41.01 32.51 64.58c17.11 18.73 32.29 33.15 46.28 44.24c3.23 2.48 7.1 3.77 10.87 3.77s7.64-1.29 10.87-3.77c14.1-11.09 29.17-25.51 46.28-44.24c21.53-23.57 32.51-45.32 32.51-64.58c0-12.27-4.74-23.9-13.46-32.83z"></path>
           </svg>
-          <button className="flex gap-2 justify-center items-center text-center border-[0.5px] px-2 py-[5px] border-black rounded-lg">
+          <button
+            className="flex gap-2 justify-center items-center text-center border-[0.5px] px-2 py-[5px] border-black rounded-lg"
+            onClick={() => setLoginModal(true)}
+          >
             <img src={User} alt="User" />
             <h3 className="font-TTHovesMedium font-[500] text-sm">Sign in</h3>
           </button>
         </div>
       </nav>
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
+      {loginModal && (
+        <LoginModal isOpen={loginModal} onClose={() => setLoginModal(false)} />
+      )}
     </>
   );
 };

8df1ebf30859fc4f8677c62a2558fdf6d1abce71
Ahmad Raza <ahmadraza20082003@gmail.com>
2025-05-24 20:40:57 +0530

    Added Admin App

---
+++ admin/src/App.js
@@ -0,0 +1,17 @@
+import logo from './assets/logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+        <p >
+          Admin Website
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
